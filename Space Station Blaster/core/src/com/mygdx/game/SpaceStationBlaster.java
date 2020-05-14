da05b2657d2dd4b20ca94318aaed28225fb1614f
Sandy Nelson <nelsy007@mymail.unisa.edu.au>
2020-05-14 18:27:17 +0930

    Created deafualt values for filters in SpaceStationBlaster

--- Space Station Blaster/core/src/com/mygdx/game/SpaceStationBlaster.java
+++ Space Station Blaster/core/src/com/mygdx/game/SpaceStationBlaster.java
@@ -10,6 +10,17 @@ public class SpaceStationBlaster extends Game {
 	public static final int V_HEIGHT = 800; // virtual width of screen;
 	public static final float PPM = 100f; // number of pixels per meter
 
+	// default values for filters
+	public static final short DEFAULT_BIT = 1;
+	public static final short PLAYER_BIT = 2;
+	public static final short ASTEROID_BIT = 4;
+	public static final short UFO_BIT = 8;
+	public static final short ENEMY_SHIP_BIT = 16;
+	public static final short PLAYER_SHOT_BIT = 32;
+	public static final short ENEMY_SHOT_BIT = 64;
+	public static final short SPACE_STATION_BIT = 128;
+	public static final short MISSILE_BIT = 256;
+
 	public SpriteBatch spriteBatch; // public to allow access to batch from SpaceStationBlaster
 									// instance
 
