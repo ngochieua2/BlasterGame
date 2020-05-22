352db4411b09a901dcfbf172ee2bcede668f26a7
Sandy Nelson <nelsy007@mymail.unisa.edu.au>
2020-05-23 01:49:03 +0930

    Created array entities to allow for max no of bullets for each ship type.

--- Space Station Blaster/core/src/com/mygdx/game/Bullets.java
+++ Space Station Blaster/core/src/com/mygdx/game/Bullets.java
@@ -2,17 +2,20 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.Screens.PlayScreen;
 
 import javax.xml.soap.Text;
 
-public class Bullets<E> {
+public class Bullets {
     private enum BulletType { FIRE, BULLET, IMPACT}
-    private enum BulletColour { GREEN, ORANGE, PURPLE, BLUE }
+    private enum ShipType { NONE, PLAYER, GREEN_UFO, RED_UFO, JUDGEMENT }
 
     // constants for green bullet
     private static final String GREEN_BULLET_TEXTURE_ATLAS_REGION = "shot-green";
@@ -27,6 +30,7 @@ public class Bullets<E> {
     private static final int GREEN_IMPACT_BULLET_END_FRAME = 9;
     private static final float GREEN_FIRE_BULLET_FRAME_DURATION = 0.1f;
     private static final float GREEN_IMPACT_BULLET_FRAME_DURATION = 0.1f;
+    private static final int GREEN_MAX_BULLETS = 4;
     private static final int GREEN_BULLET_RECTANGLE_WIDTH = 0;
     private static final int GREEN_BULLET_RECTANGLE_HEIGHT = 0;
 
@@ -43,6 +47,7 @@ public class Bullets<E> {
     private static final int ORANGE_IMPACT_BULLET_END_FRAME = 13;
     private static final float ORANGE_FIRE_BULLET_FRAME_DURATION = 0.1f;
     private static final float ORANGE_IMPACT_BULLET_FRAME_DURATION = 0.1f;
+    private static final int ORANGE_MAX_BULLETS = 2;
     private static final int ORANGE_BULLET_RECTANGLE_WIDTH = 0;
     private static final int ORANGE_BULLET_RECTANGLE_HEIGHT = 0;
 
@@ -54,11 +59,12 @@ public class Bullets<E> {
     private static final int PURPLE_BULLET_TEXTURE_HEIGHT = 128;
     private static final int PURPLE_FIRE_BULLET_START_FRAME = 0;
     private static final int PURPLE_FIRE_BULLET_END_FRAME = 3;
-    private static final int PURPLE_BULLET_FRAME = 4;
+    private static final int PURPLE_BULLET_FRAME = 1;
     private static final int PURPLE_IMPACT_BULLET_START_FRAME = 5;
     private static final int PURPLE_IMPACT_BULLET_END_FRAME = 14;
     private static final float PURPLE_FIRE_BULLET_FRAME_DURATION = 0.1f;
     private static final float PURPLE_IMPACT_BULLET_FRAME_DURATION = 0.1f;
+    private static final int PURPLE_MAX_BULLETS = 2;
     private static final int PURPLE_BULLET_RECTANGLE_WIDTH = 0;
     private static final int PURPLE_BULLET_RECTANGLE_HEIGHT = 0;
 
@@ -75,125 +81,149 @@ public class Bullets<E> {
     private static final int BLUE_IMPACT_BULLET_END_FRAME = 11;
     private static final float BLUE_FIRE_BULLET_FRAME_DURATION = 0.1f;
     private static final float BLUE_IMPACT_BULLET_FRAME_DURATION = 0.1f;
+    private static final int BLUE_MAX_BULLETS = 2;
     private static final int BLUE_BULLET_RECTANGLE_WIDTH = 0;
     private static final int BLUE_BULLET_RECTANGLE_HEIGHT = 0;
 
-    private E collider;
-    private BulletType bulletType;
-    private BulletColour bulletColour;
     private TextureAtlas textureAtlas;
     private TextureRegion textureRegion;
-    private TextureRegion bulletTextureRegion;
-    private Animation fireBulletAnimation;
-    private Animation impactBulletAnimation;
 
-    public Bullets(PlayScreen playScreen, BulletColour bulletColour) {
+    Array<TextureRegion> frames;
+
+    // bullet entities
+    private BulletType[] bulletType;
+    private ShipType[] shipTypes;
+    private Sprite[] sprite;
+    private Polygon[] collider;
+    private TextureRegion[] bulletTextureRegion;
+    private Animation[] fireBulletAnimation;
+    private Animation[] impactBulletAnimation;
+    Vector2[] position; // bullets current position
+    Vector2[] direction; // direction the bullet is travelling
+    float[] radians; // the angle in radians the bullet is
+
+    public Bullets(PlayScreen playScreen, ShipType shipType) {
         textureAtlas = playScreen.getTextureAtlas();
-        this.bulletColour = bulletColour;
-
-        switch(bulletColour) {
-            case GREEN: {
+        frames = new Array<TextureRegion>();
+        switch(shipType) {
+            case PLAYER: {
+                instantiateEntities(GREEN_MAX_BULLETS);
                 textureRegion = textureAtlas.findRegion(GREEN_BULLET_TEXTURE_ATLAS_REGION);
-                Array<TextureRegion> frames = new Array<TextureRegion>();
 
-                // get bullet texture region
-                bulletTextureRegion = new TextureRegion(textureRegion, GREEN_BULLET_TEXTURE_X,
-                        GREEN_BULLET_TEXTURE_Y, GREEN_BULLET_TEXTURE_WIDTH,
-                        GREEN_BULLET_TEXTURE_HEIGHT);
+                for (int index = 0; index < GREEN_MAX_BULLETS; index++) {
+                    clearEntities(index);
+                    // get bullet texture region
+                    bulletTextureRegion[index] = new TextureRegion(textureRegion,
+                            GREEN_BULLET_TEXTURE_X, GREEN_BULLET_TEXTURE_Y, GREEN_BULLET_TEXTURE_WIDTH,
+                            GREEN_BULLET_TEXTURE_HEIGHT);
 
-                // get the fire bullet animation frames and add them to fireBulletAnimation
-                fireBulletAnimation = createAnimation(frames, textureRegion,
-                        GREEN_FIRE_BULLET_START_FRAME, GREEN_FIRE_BULLET_END_FRAME,
-                        GREEN_BULLET_TEXTURE_X, GREEN_BULLET_TEXTURE_Y, GREEN_BULLET_TEXTURE_WIDTH,
-                        GREEN_BULLET_TEXTURE_HEIGHT, GREEN_FIRE_BULLET_FRAME_DURATION);
+                    // get the fire bullet animation frames and add them to fireBulletAnimation
+                    fireBulletAnimation[index] = createAnimation(frames, textureRegion,
+                            GREEN_FIRE_BULLET_START_FRAME, GREEN_FIRE_BULLET_END_FRAME,
+                            GREEN_BULLET_TEXTURE_X, GREEN_BULLET_TEXTURE_Y, GREEN_BULLET_TEXTURE_WIDTH,
+                            GREEN_BULLET_TEXTURE_HEIGHT, GREEN_FIRE_BULLET_FRAME_DURATION);
 
-                frames.clear();
+                    frames.clear();
 
-                // get the impact bullet animation and add them to impactBulletAnimation
-                impactBulletAnimation = createAnimation(frames, textureRegion,
-                        GREEN_IMPACT_BULLET_START_FRAME, GREEN_IMPACT_BULLET_END_FRAME,
-                        GREEN_BULLET_TEXTURE_X, GREEN_BULLET_TEXTURE_Y, GREEN_BULLET_TEXTURE_WIDTH,
-                        GREEN_BULLET_TEXTURE_HEIGHT, GREEN_IMPACT_BULLET_FRAME_DURATION);
+                    // get the impact bullet animation and add them to impactBulletAnimation
+                    impactBulletAnimation[index] = createAnimation(frames, textureRegion,
+                            GREEN_IMPACT_BULLET_START_FRAME, GREEN_IMPACT_BULLET_END_FRAME,
+                            GREEN_BULLET_TEXTURE_X, GREEN_BULLET_TEXTURE_Y, GREEN_BULLET_TEXTURE_WIDTH,
+                            GREEN_BULLET_TEXTURE_HEIGHT, GREEN_IMPACT_BULLET_FRAME_DURATION);
 
-                frames.clear();
+                    frames.clear();
+
+                    collider[index] = new Polygon(new float[]{0, 0, GREEN_BULLET_TEXTURE_WIDTH, 0,
+                            GREEN_BULLET_TEXTURE_WIDTH, GREEN_BULLET_TEXTURE_HEIGHT, 0,
+                            GREEN_BULLET_TEXTURE_HEIGHT});
+                }
                 break;
             }
-            case ORANGE: {
+            case GREEN_UFO: {
+                instantiateEntities(ORANGE_MAX_BULLETS);
                 textureRegion = textureAtlas.findRegion(ORANGE_BULLET_TEXTURE_ATLAS_REGION);
-                Array<TextureRegion> frames = new Array<TextureRegion>();
 
-                // get bullet texture region
-                bulletTextureRegion = new TextureRegion(textureRegion, ORANGE_BULLET_TEXTURE_X,
-                        ORANGE_BULLET_TEXTURE_Y, ORANGE_BULLET_TEXTURE_WIDTH,
-                        ORANGE_BULLET_TEXTURE_HEIGHT);
+                for (int index = 0; index < ORANGE_MAX_BULLETS; index++) {
+                    clearEntities(index);
+                    // get bullet texture region
+                    bulletTextureRegion[index] = new TextureRegion(textureRegion, ORANGE_BULLET_TEXTURE_X,
+                            ORANGE_BULLET_TEXTURE_Y, ORANGE_BULLET_TEXTURE_WIDTH,
+                            ORANGE_BULLET_TEXTURE_HEIGHT);
 
-                // get the fire bullet animation frames and add them to fireBulletAnimation
-                fireBulletAnimation = createAnimation(frames, textureRegion,
-                        ORANGE_FIRE_BULLET_START_FRAME, ORANGE_FIRE_BULLET_END_FRAME,
-                        ORANGE_BULLET_TEXTURE_X, ORANGE_BULLET_TEXTURE_Y, ORANGE_BULLET_TEXTURE_WIDTH,
-                        ORANGE_BULLET_TEXTURE_HEIGHT, ORANGE_FIRE_BULLET_FRAME_DURATION);
+                    // get the fire bullet animation frames and add them to fireBulletAnimation
+                    fireBulletAnimation[index] = createAnimation(frames, textureRegion,
+                            ORANGE_FIRE_BULLET_START_FRAME, ORANGE_FIRE_BULLET_END_FRAME,
+                            ORANGE_BULLET_TEXTURE_X, ORANGE_BULLET_TEXTURE_Y, ORANGE_BULLET_TEXTURE_WIDTH,
+                            ORANGE_BULLET_TEXTURE_HEIGHT, ORANGE_FIRE_BULLET_FRAME_DURATION);
 
-                frames.clear();
+                    frames.clear();
 
-                // get the impact bullet animation and add them to impactBulletAnimation
-                impactBulletAnimation = createAnimation(frames, textureRegion,
-                        ORANGE_IMPACT_BULLET_START_FRAME, ORANGE_IMPACT_BULLET_END_FRAME,
-                        ORANGE_BULLET_TEXTURE_X, ORANGE_BULLET_TEXTURE_Y, ORANGE_BULLET_TEXTURE_WIDTH,
-                        ORANGE_BULLET_TEXTURE_HEIGHT, ORANGE_IMPACT_BULLET_FRAME_DURATION);
+                    // get the impact bullet animation and add them to impactBulletAnimation
+                    impactBulletAnimation[index] = createAnimation(frames, textureRegion,
+                            ORANGE_IMPACT_BULLET_START_FRAME, ORANGE_IMPACT_BULLET_END_FRAME,
+                            ORANGE_BULLET_TEXTURE_X, ORANGE_BULLET_TEXTURE_Y, ORANGE_BULLET_TEXTURE_WIDTH,
+                            ORANGE_BULLET_TEXTURE_HEIGHT, ORANGE_IMPACT_BULLET_FRAME_DURATION);
 
-                frames.clear();
+                    frames.clear();
+                }
                 break;
             }
-            case PURPLE: {
+            case RED_UFO: {
+                instantiateEntities(PURPLE_MAX_BULLETS);
                 textureRegion = textureAtlas.findRegion(PURPLE_BULLET_TEXTURE_ATLAS_REGION);
-                Array<TextureRegion> frames = new Array<TextureRegion>();
 
-                // get bullet texture region
-                bulletTextureRegion = new TextureRegion(textureRegion, PURPLE_BULLET_TEXTURE_X,
-                        PURPLE_BULLET_TEXTURE_Y, PURPLE_BULLET_TEXTURE_WIDTH,
-                        PURPLE_BULLET_TEXTURE_HEIGHT);
+                for (int index = 0; index < PURPLE_MAX_BULLETS; index++) {
+                    clearEntities(index);
+                    // get bullet texture region
+                    bulletTextureRegion[index] =new TextureRegion(textureRegion, PURPLE_BULLET_TEXTURE_X,
+                            PURPLE_BULLET_TEXTURE_Y, PURPLE_BULLET_TEXTURE_WIDTH,
+                            PURPLE_BULLET_TEXTURE_HEIGHT);
 
-                // get the fire bullet animation frames and add them to fireBulletAnimation
-                fireBulletAnimation = createAnimation(frames, textureRegion,
-                        PURPLE_FIRE_BULLET_START_FRAME, PURPLE_FIRE_BULLET_END_FRAME,
-                        PURPLE_BULLET_TEXTURE_X, PURPLE_BULLET_TEXTURE_Y, PURPLE_BULLET_TEXTURE_WIDTH,
-                        PURPLE_BULLET_TEXTURE_HEIGHT, PURPLE_FIRE_BULLET_FRAME_DURATION);
+                    // get the fire bullet animation frames and add them to fireBulletAnimation
+                    fireBulletAnimation[index] = createAnimation(frames, textureRegion,
+                            PURPLE_FIRE_BULLET_START_FRAME, PURPLE_FIRE_BULLET_END_FRAME,
+                            PURPLE_BULLET_TEXTURE_X, PURPLE_BULLET_TEXTURE_Y, PURPLE_BULLET_TEXTURE_WIDTH,
+                            PURPLE_BULLET_TEXTURE_HEIGHT, PURPLE_FIRE_BULLET_FRAME_DURATION);
 
-                frames.clear();
+                    frames.clear();
 
-                // get the impact bullet animation and add them to impactBulletAnimation
-                impactBulletAnimation = createAnimation(frames, textureRegion,
-                        PURPLE_IMPACT_BULLET_START_FRAME, PURPLE_IMPACT_BULLET_END_FRAME,
-                        PURPLE_BULLET_TEXTURE_X, PURPLE_BULLET_TEXTURE_Y, PURPLE_BULLET_TEXTURE_WIDTH,
-                        PURPLE_BULLET_TEXTURE_HEIGHT, PURPLE_IMPACT_BULLET_FRAME_DURATION);
+                    // get the impact bullet animation and add them to impactBulletAnimation
+                    impactBulletAnimation[index] = createAnimation(frames, textureRegion,
+                            PURPLE_IMPACT_BULLET_START_FRAME, PURPLE_IMPACT_BULLET_END_FRAME,
+                            PURPLE_BULLET_TEXTURE_X, PURPLE_BULLET_TEXTURE_Y, PURPLE_BULLET_TEXTURE_WIDTH,
+                            PURPLE_BULLET_TEXTURE_HEIGHT, PURPLE_IMPACT_BULLET_FRAME_DURATION);
 
-                frames.clear();
+                    frames.clear();
+                }
                 break;
             }
-            case BLUE: {
+            case JUDGEMENT: {
+                instantiateEntities(BLUE_MAX_BULLETS);
                 textureRegion = textureAtlas.findRegion(BLUE_BULLET_TEXTURE_ATLAS_REGION);
-                Array<TextureRegion> frames = new Array<TextureRegion>();
 
-                // get bullet texture region
-                bulletTextureRegion = new TextureRegion(textureRegion, BLUE_BULLET_TEXTURE_X,
-                        BLUE_BULLET_TEXTURE_Y, BLUE_BULLET_TEXTURE_WIDTH,
-                        BLUE_BULLET_TEXTURE_HEIGHT);
+                for (int index = 0; index < PURPLE_MAX_BULLETS; index++) {
+                    clearEntities(index);
+                    // get bullet texture region
+                    bulletTextureRegion[index] = new TextureRegion(textureRegion, BLUE_BULLET_TEXTURE_X,
+                            BLUE_BULLET_TEXTURE_Y, BLUE_BULLET_TEXTURE_WIDTH,
+                            BLUE_BULLET_TEXTURE_HEIGHT);
 
-                // get the fire bullet animation frames and add them to fireBulletAnimation
-                fireBulletAnimation = createAnimation(frames, textureRegion,
-                        BLUE_FIRE_BULLET_START_FRAME, BLUE_FIRE_BULLET_END_FRAME,
-                        BLUE_BULLET_TEXTURE_X, BLUE_BULLET_TEXTURE_Y, BLUE_BULLET_TEXTURE_WIDTH,
-                        BLUE_BULLET_TEXTURE_HEIGHT, BLUE_FIRE_BULLET_FRAME_DURATION);
+                    // get the fire bullet animation frames and add them to fireBulletAnimation
+                    fireBulletAnimation[index] = createAnimation(frames, textureRegion,
+                            BLUE_FIRE_BULLET_START_FRAME, BLUE_FIRE_BULLET_END_FRAME,
+                            BLUE_BULLET_TEXTURE_X, BLUE_BULLET_TEXTURE_Y, BLUE_BULLET_TEXTURE_WIDTH,
+                            BLUE_BULLET_TEXTURE_HEIGHT, BLUE_FIRE_BULLET_FRAME_DURATION);
 
-                frames.clear();
+                    frames.clear();
 
-                // get the impact bullet animation and add them to impactBulletAnimation
-                impactBulletAnimation = createAnimation(frames, textureRegion,
-                        BLUE_IMPACT_BULLET_START_FRAME, BLUE_IMPACT_BULLET_END_FRAME,
-                        BLUE_BULLET_TEXTURE_X, BLUE_BULLET_TEXTURE_Y, BLUE_BULLET_TEXTURE_WIDTH,
-                        BLUE_BULLET_TEXTURE_HEIGHT, BLUE_IMPACT_BULLET_FRAME_DURATION);
+                    // get the impact bullet animation and add them to impactBulletAnimation
+                    impactBulletAnimation[index] = createAnimation(frames, textureRegion,
+                            BLUE_IMPACT_BULLET_START_FRAME, BLUE_IMPACT_BULLET_END_FRAME,
+                            BLUE_BULLET_TEXTURE_X, BLUE_BULLET_TEXTURE_Y, BLUE_BULLET_TEXTURE_WIDTH,
+                            BLUE_BULLET_TEXTURE_HEIGHT, BLUE_IMPACT_BULLET_FRAME_DURATION);
 
-                frames.clear();
+                    frames.clear();
+                }
                 break;
             }
         }
@@ -210,7 +240,26 @@ public class Bullets<E> {
         return new Animation(frameDuration, frames);
     }
 
-    public void spawn(E collider, float angle) {
+    private void instantiateEntities(int maxSize) {
+        shipTypes = new ShipType[maxSize];
+        position = new Vector2[maxSize];
+        direction = new Vector2[maxSize];
+        radians = new float[maxSize];
+    }
+
+    private void clearEntities(int index) {
+        shipTypes[index] = ShipType.NONE;
+        position[index] = new Vector2();
+        direction[index] = new Vector2();
+        radians[index] = 0f;
+    }
+
+
+
+    public void spawn(Vector2 position, float angleRadians) {
+        //this.position = position;
+        //radians = angleRadians;
+
 
     }
 }
