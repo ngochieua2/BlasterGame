e3eb20a93dca3c6fe40e215f82c2fd167d398dba
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-05-25 20:29:00 +0930

    upload Asteroids class

--- Space Station Blaster/core/src/com/mygdx/game/Tools/Asteroids.java
+++ Space Station Blaster/core/src/com/mygdx/game/Tools/Asteroids.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.Tools;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
@@ -8,59 +9,195 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.Screens.PlayScreen;
 import com.mygdx.game.Sprites.LargeBrownAsteroids;
+import com.mygdx.game.Sprites.LargeGreyAsteroids;
+import com.mygdx.game.Sprites.MediumBrownAsteroids;
+import com.mygdx.game.Sprites.MediumGreyAsteroids;
+import com.mygdx.game.Sprites.SmallBrownAsteroids;
+import com.mygdx.game.Sprites.SmallGreyAsteroids;
 
-public class AsteroidTools {
+public class Asteroids {
     private static final int Asteroids_Max = 20;
 
-    private static final float Asteroids_Density = 0.4f;
+    private static final float MAX_TIME_SPAWN = 10f;
+    private static final float MIN_TIME_SPAWN = 1f;
 
 
-    private World world;
     private PlayScreen playScreen;
-    private Sprite LBrown, MBrown, SBrown, LGrey, MGrey, SGrey;
+    private Sprite[] asteroidSprite;
 
-    private float x,y;
-    private final Sprite type;
-    private float radians;
+    private enum  TYPE {BROWN_LARGE, GREY_LARGE, BROWN_MEDIUM, GREY_MEDIUM, BROWN_SMALL, GREY_SMALL, NONE};
+    private TYPE[] type;
+    private float x;
+    private float y;
+    private float[] radians;
     private float VectorX;
     private float VectorY;
-    private Vector2 position;
-    private int width;
-    private int height;
-
-    private float health;
-    private Body body;
-
-    private int speed;
+    private Vector2[] position;
+    private Vector2[] direction;
 
 
-    public AsteroidTools(World world, PlayScreen playScreen, float x, float y, Sprite type){
-        this.world = world;
+    private float[] health;
+
+    private float speed;
+    private float timeSpawn = MathUtils.random(MIN_TIME_SPAWN, MAX_TIME_SPAWN);;
+
+
+    public Asteroids(PlayScreen playScreen, TYPE t){
         this.playScreen = playScreen;
-        this.x = x;
-        this.y = y;
-        this.type = type;
 
-        LBrown = new LargeBrownAsteroids(world,playScreen);
-        LGrey = new LargeBrownAsteroids(world,playScreen);
 
-        if (type == LBrown || type == LGrey){
+        position = new Vector2[Asteroids_Max];
+        direction = new Vector2[Asteroids_Max];
+        radians = new float[Asteroids_Max];
 
-            width = 101; health = 84;
-            speed = 32;
+        int index = -1;
+        for (int i = 0; i < Asteroids_Max; i++){
+            if (type[i] == TYPE.NONE){
+                index = i;
+            }
         }
 
-        radians = MathUtils.random(2 * 3.14f);
-        VectorX = MathUtils.cos(radians) * speed;
-        VectorY = MathUtils.sin(radians) * speed;
-        position = new Vector2(x,y);
-        body = ShapeFactory.createCircle(position, width / 2, BodyDef.BodyType.DynamicBody, world, 0.4f);
+        type[index] = t;
+        switch (t){
+            case BROWN_LARGE:
+                Sprite LBrown = new LargeBrownAsteroids(playScreen);
+                asteroidSprite[index] = new Sprite(LBrown);
+                speed = 32f;
+                health[index] = 3;
+
+                //Spawn random in x-axis
+                VectorX = MathUtils.random(0 - LBrown.getRegionWidth(), Gdx.graphics.getWidth() + LBrown.getRegionWidth());
+                //if asteroids spawn both sides of screen, Y position will spawn random
+                if (VectorX < 0 + LBrown.getRegionWidth()/2 || VectorX > Gdx.graphics.getWidth() - LBrown.getRegionWidth()/2){
+                    VectorY = MathUtils.random(0 - LBrown.getRegionHeight(), Gdx.graphics.getHeight() + LBrown.getRegionHeight());
+                }
+                //Or Asteroids only spawn on the top of screen
+                else {
+                    VectorY = Gdx.graphics.getHeight() + LBrown.getRegionHeight();
+                }
+
+                asteroidSprite[index].setOrigin(VectorX,VectorY);
+                asteroidSprite[index].setCenter(VectorX,VectorY);
+
+                break;
+
+            case GREY_LARGE:
+                Sprite LGrey = new LargeGreyAsteroids(playScreen);
+                asteroidSprite[index] = new Sprite(LGrey);
+                speed = 32f;
+                health[index] = 3;
+
+                //Spawn random in x-axis
+                VectorX = MathUtils.random(0 - LGrey.getRegionWidth(), Gdx.graphics.getWidth() + LGrey.getRegionWidth());
+                //if asteroids spawn both sides of screen, Y position will spawn random
+                if (VectorX < 0 + LGrey.getRegionWidth()/2 || VectorX > Gdx.graphics.getWidth() - LGrey.getRegionWidth()/2){
+                    VectorY = MathUtils.random(0 - LGrey.getRegionHeight(), Gdx.graphics.getHeight() + LGrey.getRegionHeight());
+                }
+                //Or Asteroids only spawn on the top of screen
+                else {
+                    VectorY = Gdx.graphics.getHeight() + LGrey.getRegionHeight();
+                }
+
+                asteroidSprite[index].setOrigin(VectorX,VectorY);
+                asteroidSprite[index].setCenter(VectorX,VectorY);
+                break;
+
+            case BROWN_MEDIUM:
+                Sprite M_Brown = new MediumBrownAsteroids(playScreen);
+                asteroidSprite[index] = new Sprite(M_Brown);
+                speed = 32f;
+                health[index] = 3;
+
+                //Spawn random in x-axis
+                VectorX = MathUtils.random(0 - M_Brown.getRegionWidth(), Gdx.graphics.getWidth() + M_Brown.getRegionWidth());
+                //if asteroids spawn both sides of screen, Y position will spawn random
+                if (VectorX < 0 + M_Brown.getRegionWidth()/2 || VectorX > Gdx.graphics.getWidth() - M_Brown.getRegionWidth()/2){
+                    VectorY = MathUtils.random(0 - M_Brown.getRegionHeight(), Gdx.graphics.getHeight() + M_Brown.getRegionHeight());
+                }
+                //Or Asteroids only spawn on the top of screen
+                else {
+                    VectorY = Gdx.graphics.getHeight() + M_Brown.getRegionHeight();
+                }
+
+                asteroidSprite[index].setOrigin(VectorX,VectorY);
+                asteroidSprite[index].setCenter(VectorX,VectorY);
+                break;
+
+            case GREY_MEDIUM:
+                Sprite M_Grey = new MediumGreyAsteroids(playScreen);
+                asteroidSprite[index] = new Sprite(M_Grey);
+                speed = 32f;
+                health[index] = 3;
+
+                //Spawn random in x-axis
+                VectorX = MathUtils.random(0 - M_Grey.getRegionWidth(), Gdx.graphics.getWidth() + M_Grey.getRegionWidth());
+                //if asteroids spawn both sides of screen, Y position will spawn random
+                if (VectorX < 0 + M_Grey.getRegionWidth()/2 || VectorX > Gdx.graphics.getWidth() - M_Grey.getRegionWidth()/2){
+                    VectorY = MathUtils.random(0 - M_Grey.getRegionHeight(), Gdx.graphics.getHeight() + M_Grey.getRegionHeight());
+                }
+                //Or Asteroids only spawn on the top of screen
+                else {
+                    VectorY = Gdx.graphics.getHeight() + M_Grey.getRegionHeight();
+                }
+
+                asteroidSprite[index].setOrigin(VectorX,VectorY);
+                asteroidSprite[index].setCenter(VectorX,VectorY);
+                break;
+
+            case BROWN_SMALL:
+                Sprite S_Brown = new SmallBrownAsteroids(playScreen);
+                asteroidSprite[index] = new Sprite(S_Brown);
+                speed = 32f;
+                health[index] = 3;
+
+                //Spawn random in x-axis
+                VectorX = MathUtils.random(0 - S_Brown.getRegionWidth(), Gdx.graphics.getWidth() + S_Brown.getRegionWidth());
+                //if asteroids spawn both sides of screen, Y position will spawn random
+                if (VectorX < 0 + S_Brown.getRegionWidth()/2 || VectorX > Gdx.graphics.getWidth() - S_Brown.getRegionWidth()/2){
+                    VectorY = MathUtils.random(0 - S_Brown.getRegionHeight(), Gdx.graphics.getHeight() + S_Brown.getRegionHeight());
+                }
+                //Or Asteroids only spawn on the top of screen
+                else {
+                    VectorY = Gdx.graphics.getHeight() + S_Brown.getRegionHeight();
+                }
+
+                asteroidSprite[index].setOrigin(VectorX,VectorY);
+                asteroidSprite[index].setCenter(VectorX,VectorY);
+                break;
+
+            case GREY_SMALL:
+                Sprite S_Grey = new SmallGreyAsteroids(playScreen);
+                asteroidSprite[index] = new Sprite(S_Grey);
+                speed = 32f;
+                health[index] = 3;
+
+                //Spawn random in x-axis
+                VectorX = MathUtils.random(0 - S_Grey.getRegionWidth(), Gdx.graphics.getWidth() + S_Grey.getRegionWidth());
+                //if asteroids spawn both sides of screen, Y position will spawn random
+                if (VectorX < 0 + S_Grey.getRegionWidth()/2 || VectorX > Gdx.graphics.getWidth() - S_Grey.getRegionWidth()/2){
+                    VectorY = MathUtils.random(0 - S_Grey.getRegionHeight(), Gdx.graphics.getHeight() + S_Grey.getRegionHeight());
+                }
+                //Or Asteroids only spawn on the top of screen
+                else {
+                    VectorY = Gdx.graphics.getHeight() + S_Grey.getRegionHeight();
+                }
+
+                asteroidSprite[index].setOrigin(VectorX,VectorY);
+                asteroidSprite[index].setCenter(VectorX,VectorY);
+                break;
+
+            case NONE:
+                break;
+
+        }
+
     }
 
+
+
     public void update(float dt){
-        x += VectorX * dt;
-        y += VectorY * dt;
-        Vector2 position = new Vector2(x,y);
+
+
 
     }
 
